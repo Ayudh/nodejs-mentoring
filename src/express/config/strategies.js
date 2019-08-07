d9d2434eaa4c83d086e47339240b59fd7d823e06
Hari G <ghari81189@gmail.com>
2019-08-07 13:21:14 +0530

    add google, facebook, local strategy for authentication

---
+++ src/express/config/strategies.js
@@ -0,0 +1,12 @@
+export const facebook = {
+  clientID: '122508448613370',
+  clientSecret: '8b438d1c9e30ee75d33478ff7fa88d35',
+  callbackUrl: 'http://localhost:3000/auth/facebook/callback'
+};
+
+export const google = {
+  clientID:
+    '337948104878-n677jofrklkmlp445p52uc79qa3rvck7.apps.googleusercontent.com',
+  clientSecret: '_mhYliIdWR6BdruUuqycHgrL',
+  callbackUrl: 'http://localhost:3000/auth/google/callback'
+};
--- src/express/routes/auth/index.js
+++ src/express/routes/auth/index.js
@@ -1,26 +1,59 @@
 import express from 'express';
 import user from '../../config/user.json';
-import secretConfig from '../../config/secret.json';
-import * as jwt from 'jsonwebtoken';
+import passport from 'passport';
+import { Strategy as LocalStrategy } from 'passport-local';
+import {
+  facebookStrategy,
+  googleStrategy,
+  localStrategy
+} from '../../strategies';
+
+export const authByPassport = type => (req, res) => {
+  passport.authenticate(type, (err, user, info) => {
+    if (err) {
+      return res.send(err);
+    }
+    return res.send(info);
+  })(req, res);
+};
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  const { username, password } = req.body;
-  if (username === user.username && password === user.password) {
-    const token = jwt.sign(username, secretConfig.secret);
-    return res.json({
-      code: 200,
-      message: 'OK',
-      data: user,
-      token: token
-    });
-  } else {
-    res.json({
-      code: 404,
-      message: 'Not Found'
-    });
-  }
+passport.use('facebook', facebookStrategy());
+passport.use('google', googleStrategy());
+passport.use('local', localStrategy());
+
+passport.use(
+  new LocalStrategy(function(username, password, done) {
+    const { username: configUsername, password: configPassword } = user;
+    if (username !== configUsername || password !== configPassword)
+      return done(null, false);
+    else return done(null, user);
+  })
+);
+
+router.post('/login', passport.authenticate('local'), function(req, res) {
+  res.redirect('/');
 });
+
+router
+  .route('/local')
+  .get(
+    passport.authenticate('local', { failureRedirect: '/auth/local' }),
+    function(req, res) {
+      res.redirect('/');
+    }
+  );
+
+router.route('/facebook').get(passport.authenticate('facebook'));
+router.route('/facebook/callback').get(authByPassport('facebook'));
+
+router.route('/twitter').get(passport.authenticate('twitter'));
+router.route('/twitter/callback').get(authByPassport('twitter'));
+
+router
+  .route('/google')
+  .get(passport.authenticate('google', { scope: ['profile'] }));
+router.route('/google/callback').get(authByPassport('google'));
 
 export { router };
